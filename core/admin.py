47ec427f7cdcd2178486144b2fa7e444275519d9
Parthiban <rp.paarthi@gmail.com>
2026-06-08 19:01:01 +0530

    Add models, admin config, initial migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Document, Comment
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role', {'fields': ('role', )}),
+    )
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uploaded_by', 'assigned_to', 'status', 'date_uploaded')
+    list_editable = ('status',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'document', 'comment_date')
--- core/models.py
+++ core/models.py
@@ -3,4 +3,32 @@ from django.db import models
 
 
 class User(AbstractUser):
-    ROLE_CHOICES = [('none', 'None'), ('submitter', 'Submitter'), ('reviewer', 'Reviewer')]
+    ROLE_CHOICES = [
+        ('none', 'None'), 
+        ('submitter', 'Submitter'), 
+        ('reviewer', 'Reviewer')
+        ]
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='none')
+
+class Document(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'), 
+        ('under_review', 'Under Review'), 
+        ('approved', 'Approved'), 
+        ('rejected', 'Rejected')
+        ]
+
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='documents/')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='draft')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uploaded_documents')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_documents')
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_reviewed = models.DateField(null=True, blank=True)
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    comment_date = models.DateField(auto_now_add=True)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='comments')
